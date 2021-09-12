3cfb1dc01edf26a004c750f60d77518686093967
u_dev <uddhavdave1@gmail.com>
2021-09-12 13:11:30 +0530

    Added Undefeatable AI player

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate rand;
 extern crate termcolor;
 
 use std::io::Write;
@@ -45,6 +46,100 @@ fn draw(state: &[char]) {
     }
 
     println!("-------------");
+}
+
+fn random_move(state: &[char]) -> usize {
+    loop {
+        let pos = rand::random::<usize>() % 10;
+        if state[pos] != 'O' && state[pos] != 'X' {
+            return pos;
+        }
+    }
+}
+
+fn block_move(state: &[char]) -> Option<usize> {
+    for tmp in 0..3 {
+        if state[tmp] == state[tmp + 3]
+            || state[tmp] == state[tmp + 6]
+            || state[tmp + 3] == state[tmp + 6]
+        {
+            for x in [0, 3, 6].iter() {
+                if state[tmp + x] != 'O' && state[tmp + x] != 'X' {
+                    return Some(tmp + x);
+                }
+            }
+        }
+
+        let tmp = tmp * 3;
+
+        if state[tmp] == state[tmp + 1]
+            || state[tmp] == state[tmp + 2]
+            || state[tmp + 1] == state[tmp + 2]
+        {
+            for x in [0, 1, 2].iter() {
+                if state[tmp + x] != 'O' && state[tmp + x] != 'X' {
+                    return Some(tmp + x);
+                }
+            }
+        }
+    }
+
+    if state[0] == state[4] || state[0] == state[8] || state[4] == state[8] {
+        for idx in [0 as usize, 4, 8].iter() {
+            if state[*idx] != 'O' && state[*idx] != 'X' {
+                return Some(*idx);
+            }
+        }
+    }
+
+    if state[2] == state[4] || state[2] == state[6] || state[4] == state[6] {
+        for idx in [2 as usize, 4, 6].iter() {
+            if state[*idx] != 'O' && state[*idx] != 'X' {
+                return Some(*idx);
+            }
+        }
+    }
+    None
+}
+
+fn random_corner(state: &[char], corners_idx: &[usize; 4]) -> Option<usize> {
+    for corner in corners_idx {
+        if state[*corner] != 'O' && state[*corner] != 'X' {
+            return Some(*corner);
+        }
+    }
+    None
+}
+
+fn ai_move(state: &[char], turn: usize) -> usize {
+    let corners = ['1', '3', '7', '9'];
+    let corners_idx = [0, 2, 6, 8];
+    //We assign higher priority to corners
+    match turn {
+        1 => match random_corner(state, &corners_idx) {
+            Some(x) => x,
+            None => random_move(state),
+        },
+        2 => {
+            for (idx, targets) in corners_idx.iter().enumerate() {
+                //Check if player played a corner
+                if state[*targets] != corners[idx] {
+                    return 4;
+                }
+            }
+            match random_corner(state, &corners_idx) {
+                Some(x) => x,
+                None => random_move(state),
+            }
+        }
+        _ => match block_move(state) {
+            None => match random_corner(state, &corners_idx) {
+                Some(x) => x,
+                None => random_move(state),
+            },
+            Some(x) => x,
+        },
+    }
 }
 
 fn ask_user(state: &mut [char], player: char) {
@@ -118,12 +213,35 @@ fn main() {
     // Welcome the player
     greeting();
 
+    let ai_player;
+    let mut user_move = match rand::random::<usize>() % 2 {
+        0 => {
+            println!(" AI goes second.");
+            ai_player = 'O';
+            true
+        }
+        _ => {
+            println!(" AI goes first.");
+            ai_player = 'X';
+            false
+        }
+    };
+
+    let mut turn: usize = 0;
     loop {
         // Draw the field
         draw(&state);
 
-        // Ask for user input
-        ask_user(&mut state, player);
+        turn += 1;
+        if user_move {
+            // Ask for user input
+            ask_user(&mut state, player);
+            user_move = false;
+        } else {
+            println!("AI made the Move 🤖");
+            state[ai_move(&state, turn)] = ai_player;
+            user_move = true;
+        }
 
         // Check if a player won
         if has_won(&state) {
